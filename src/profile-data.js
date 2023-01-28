db648d71307a938e96a954517d8580eefbcc1255
Silas <ojugos93@gmail.com>
2023-01-28 11:57:36 +0100

    first commit

---
+++ src/profile-data.js
@@ -0,0 +1,23 @@
+export const profiles = [
+  {
+    img: "https://i.ibb.co/gvwfFJK/profile1.png",
+    name: "Adaora Nwodo",
+    job: "Cloud Engineer",
+    company: "Microsoft",
+    link: "https://twitter.com/AdoraNwodo",
+  },
+  {
+    img: "https://i.ibb.co/7W7rV8v/profile2.png",
+    name: "John Doe",
+    job: "Web Developer",
+    company: "Facebook",
+    link: "#",
+  },
+  {
+    img: "https://i.ibb.co/tKn71dn/profile3.png",
+    name: "Jennifer Winslow",
+    job: "Mobile Developer",
+    company: "Ojtech Media",
+    link: "#",
+  },
+];
